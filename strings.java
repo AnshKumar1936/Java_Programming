7801e4342ddd4aed9b5f891eb9a60903dfa5b48b
Ansh Kumar Singh <148905627+AnshKumar1936@users.noreply.github.com>
2024-06-01 18:30:03 +0530

    Add files via upload

---
+++ strings.java
@@ -0,0 +1,40 @@
+import java.util.*;
+public class strings {
+    public static void main(String[] args) {
+        // String name="Ansh";
+        // String sirname= "Kumar";
+        // System.out.print(name);
+        // System.out.print(sirname);
+
+        //concatenation to add strings
+
+        // String fullname= name+sirname;
+        // System.out.println(fullname);
+
+        //length of String
+
+        // System.out.println(fullname.length());
+
+        //charAt to print alphabets
+        
+        // for(int i=0;i<fullname.length();i++){
+        //     System.out.println(fullname.charAt(i));
+        // }
+
+        // //compare strings
+
+        // if(name.compareTo(sirname)==0){
+        //     System.out.print("strings are equal");
+
+        // }
+        // else{
+        //     System.out.println("strings are not equal");
+        // }
+
+        String sentence= "My name is ansh";
+        String name=sentence.substring(0);
+        System.out.println(name);
+
+    }
+    
+}
